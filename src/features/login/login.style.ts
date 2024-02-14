61398db534dc20cc58c9efb39d56696e5946bc41
Pierry Leal <pierry_leal@hotmail.com>
2024-02-14 13:47:14 -0300

    refeat: nomes dos arquivos

--- src/features/login/login.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const LoginStyle = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  padding: 1.2rem;
-  background-color: ${({ theme }) => theme.light_gray};
-`;
--- src/features/shared/shared.consts.ts
+++
@@ -1,8 +0,0 @@
-export const Themes = {
-  primary: '#074458',
-  medium_primary: '#074458AB',
-  light_primary: 'rgba(7, 68, 88, 0.26)',
-  white: '#FFFFFF',
-  gray: '#C2BEBE',
-  light_gray: '#E4E4E4',
-};
