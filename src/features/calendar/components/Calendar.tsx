4531ffc7ca3b03c376befa7ada3a0963eb17897e
Pierry Leal <pierry_leal@hotmail.com>
2024-02-26 18:58:15 -0300

    feat: adicionar tela de calendario academico

--- src/features/calendar/components/Calendar.tsx
+++ src/features/calendar/components/Calendar.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { CalendarContainer as S } from '../Calendar.style';
+import calendar_2024 from '../../../assets/calendario_academico_2024-1.png';
 
 const Calendar: React.FC = () => {
-  return <div>Calendar</div>;
+  return (
+    <S.Container>
+      <S.Content>
+        <img src={calendar_2024} width={'100%'} />
+      </S.Content>
+    </S.Container>
+  );
 };
 
 export default Calendar;
