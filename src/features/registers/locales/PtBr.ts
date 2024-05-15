4c0e451b61a5b115abfdb4747cff12dbc7e799dc
Mateus Abu Kamel <88752743+MateusAbu@users.noreply.github.com>
2024-05-15 13:06:37 -0300

    Fix ExportButton style and registerCards order

--- src/features/registers/locales/PtBr.ts
+++ src/features/registers/locales/PtBr.ts
@@ -8,7 +8,25 @@ const PtBr = {
   SUBTITLEMATRIZ: 'Cadastro de Matriz Curricular',
   SUCCESSTITLEMATRIZ: 'Matriz Curricular Cadastrada com Sucesso!',
   SUCCESSMESSAGEMATRIZ:
+    'Agora você pode cadastrar uma Turma. Por meio do menu Cadastrar uma Turma.',
+
+  CARDTITLECLASS: 'Cadastre uma Turma',
+  CARDDESCRIPTIONCLASS:
+    'Selecione essa opção para acessar as ferramentas de cadastro de turma.',
+  TITLECLASS: 'CADASTROS > TURMA',
+  SUBTITLECLASS: 'Cadastro de Turma',
+  SUCCESSTITLECLASS: 'Turma Cadastrada com Sucesso!',
+  SUCCESSMESSAGECLASS:
     'Agora você pode cadastrar os professores para adicioná-los as disciplinias em seguida. Por meio do menu Cadastrar Professor.',
+
+  CARDTITLETEACHER: 'Cadastre um Professor',
+  CARDDESCRIPTIONTEACHER:
+    'Selecione essa opção para acessar as ferramentas de cadastro de professor.',
+  TITLETEACHER: 'CADASTROS > PROFESSOR',
+  SUBTITLETEACHER: 'Cadastro de Professor',
+  SUCCESSTITLETEACHER: 'Professor Cadastrado com Sucesso!',
+  SUCCESSMESSAGETEACHER:
+    'Agora você pode cadastrar um professor nas disciplinas Cadastrada. Por meio do menu Cadastrar Disciplinas.',
 
   CARDTITLEDISCIPLINES: 'Cadastrar ou Editar Disciplinas',
   CARDDESCRIPTIONDISCIPLINES:
@@ -19,23 +37,6 @@ const PtBr = {
   SUCCESSMESSAGEDISCIPLINES:
     'Agora as disciplinas estão cadastradas e disponíveis para fazer os horários.',
 
-  CARDTITLETEACHER: 'Cadastre um Professor',
-  CARDDESCRIPTIONTEACHER:
-    'Selecione essa opção para acessar as ferramentas de cadastro de professor',
-  TITLETEACHER: 'CADASTROS > PROFESSOR',
-  SUBTITLETEACHER: 'Cadastro de Professor',
-  SUCCESSTITLETEACHER: 'Professor Cadastrado com Sucesso!',
-  SUCCESSMESSAGETEACHER:
-    'Agora você pode cadastrar um professor nas disciplinas Cadastrada. Por meio do menu Cadastrar Disciplinas.',
-
-  CARDTITLECLASS: 'Cadastre uma Turma',
-  CARDDESCRIPTIONCLASS:
-    'Selecione essa opção para acessar as ferramentas de cadastro de turma',
-  TITLECLASS: 'CADASTROS > TURMA',
-  SUBTITLECLASS: 'Cadastro de Turma',
-  SUCCESSTITLECLASS: 'Turma Cadastrada com Sucesso!',
-  SUCCESSMESSAGECLASS:
-    'Agora a turma está cadastrada e disponível para montar os horários.',
 };
 
 export default PtBr;
